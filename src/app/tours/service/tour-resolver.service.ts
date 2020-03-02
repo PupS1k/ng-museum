f62a8f5dc8fc91ae056961ab71f6183395d256df
YTeH0k <denikpro@mail.ru>
2020-03-02 12:23:42 +0300

    create resolvers for tours

--- src/app/tours/service/tour-resolver.service.ts
+++ src/app/tours/service/tour-resolver.service.ts
@@ -1,13 +1,14 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs';
+
 import {Tour} from '../../exhibits/models/tour.model';
 import {ToursService} from './tours.service';
 
 
 
 @Injectable({ providedIn: 'root' })
-export class ExhibitsResolverService implements Resolve<Observable<Tour>> {
+export class TourResolverService implements Resolve<Observable<Tour>> {
   constructor(private toursService: ToursService) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any>|Promise<any>|any {
